730c8f754f18b6347ac252dd3813fb968138e1f0
cbeausei <clementbeauseigneur@hotmail.fr>
2016-02-16 20:42:40 +0100

    Code clément modified

--- clement.py
+++ clement.py
@@ -1,5 +1,6 @@
 from heapq import *
-from math import sqrt
+from math import sqrt,ceil
+import sys
 
 r,c,d,t,pds=map(int,input().split())
 p=int(input())
@@ -9,153 +10,123 @@ plt.sort()
 w=int(input())
 wl=[(-1,-1,[]) for i in range(w)]
 for i in range(w):
-	a,b=map(int,input().split())
-	l=[int(j) for j in input().split()]
-	wl[i]=(a,b,l)
+    a,b=map(int,input().split())
+    l=[int(j) for j in input().split()]
+    wl[i]=(a,b,l)
 o=int(input())
-ol=[(-1,-1,-1,[]) for i in range(o)]
+ol=[(-1,-1,[]) for i in range(o)]
 for i in range(o):
-	a,b=map(int,input().split())
-	nb=int(input())
-	l=[int(j) for j in input().split()]
-	v=[0 for j in range(p)]
-	for x in l:
-		v[x]+=1
-	ol[i]=(a,b,v)
+    a,b=map(int,input().split())
+    nb=int(input())
+    l=[int(j) for j in input().split()]
+    v=[0 for j in range(p)]
+    for j in l:
+        v[j]+=1
+    ol[i]=(a,b,v)
 
-pq=[]
+def dist(a,b,x,y):
+    return(ceil(sqrt((a-x)**2+(b-y)**2)))
+    
+pq_drone=[]
 for i in range(d):
-	heappush(pq,(0,i,wl[0][0],wl[0][1]))
+    heappush(pq_drone,(0,i,wl[0][0],wl[0][1]))
 
-needed=[0 for i in range(p)]
-for _,_,v in ol:
-	for j in range(p):
-		needed[j]+=v[j]
+pq_order=[]
+for i in range(o):
+    weight=0
+    _,_,l=ol[i]
+    for j in l:
+        weight+=pl[j]
+    heappush(pq_order,(weight,i))
 
-load=[[0 for j in range(p)] for i in range(d)]
+current=-1
+order_x=-1
+order_y=-1
+order_l=[]
 
-def warehouse(a,b):
-	i=-1
-	for x,y,_ in wl:
-		i+=1
-		if x==a and y==b:
-			return(i)
-	return(-1)
-
-def puissance_warehouse(i,a,b):
-	x,y,l=wl[i]
-	nb=0
-	for j in range(p):
-		nb+=min(l[j],needed[j])
-	dist=round(sqrt((a-x)**2+(b-y)**2))
-	return(nb/dist)
-
-def best_warehouse(a,b):
-	best=-1
-	puissance=-1
-	for i in range(w):
-		puissance_temp=puissance_warehouse(i,a,b)
-		if puissance_temp>puissance:
-			best=i
-			puissance=puissance_temp
-	return(best)
-
-def best_client(nd,a,b):
-	bx=-1
-	by=-1
-	nbb=-1
-	for i in range(o):
-		nb=0
-		x,y,v=ol[i]
-		for j in range(p):
-			nb+=min(load[nd][j],v[j])
-		if nb>nbb and x*y!=a*b:
-			nbb=nb
-			bx=x
-			by=y
-	return(bx,by)
-	
-def client_in(nd,a,b):
-	for i in range(o):
-		x,y,_=ol[i]
-		if x==a and y==b and nb_deliver_client(nd,i)>0:
-			return(i)
-	return(-1)
-
-def nb_deliver_client(nd,i):
-	nb=0
-	_,_,v=ol[i]
-	for j in range(p):
-		nb+=min(load[nd][j],v[j])
-	#print('nb deliver',nd,i,nb)
-	return(nb)
-
-def deliver_client(nd,client,tt):
-	global com,nb_com
-	x,y,v=ol[client]
-	nb_max=-1
-	item=-1
-	for j in range(p):
-		nb=min(load[nd][j],v[j])
-		if nb>nb_max:
-			nb_max=nb
-			item=j
-	com+=str(nd)+' D '+str(client)+' '+str(item)+' '+str(nb_max)+'\n'
-	nb_com+=1
-	load[nd][item]-=nb_max
-	v[item]-=nb_max
-	ol[client]=(x,y,v)
-
-def load_best(nd,ware):
-	global com,nb_com
-	nb=0
-	free=pds
-	x,y,l=wl[ware]
-	for poids,i in plt:
-		u=min(min(free//poids,needed[i]),l[i])
-		if u>0:
-			free-=u*poids
-			com+=str(nd)+' L '+str(ware)+' '+str(i)+' '+str(u)+'\n'
-			nb_com+=1
-			load[nd][i]+=u
-			needed[i]-=u
-			l[i]-=u
-			nb+=1
-	wl[ware]=(x,y,l)
-	return(nb)
-
-t=3000
-com=""
 nb_com=0
+com=""
+nnb=0
+#t=200
+
+xx=sum(wl[0][2])
 
 while(True):
-	tt,nd,a,b=heappop(pq)
-	if tt>=t:break
-	
-	ware=warehouse(a,b)
-	if sum(load[nd])!=0:
-		#print('a')
-		client=client_in(nd,a,b)
-		nb=0
-		if client>=0:nb=nb_deliver_client(nd,client)
-		if nb<=0:
-			x,y=best_client(nd,a,b)
-			dist=round(sqrt((a-x)**2+(b-y)**2))
-			heappush(pq,(tt+dist,nd,x,y))
-		else:
-			deliver_client(nd,client,tt)
-			heappush(pq,(tt+1,nd,a,b))
-	elif ware==-1:
-		#print('b')
-		ware=best_warehouse(a,b)
-		x,y,_=wl[ware]
-		dist=round(sqrt((a-x)**2+(b-y)**2))
-		heappush(pq,(tt+dist,nd,x,y))
-	else:
-		#print('c')
-		nb=load_best(nd,ware)
-		heappush(pq,(tt+nb,nd,a,b))		
-
+    tt,nd,a,b=heappop(pq_drone)
+    # print("Drone :",nd,file=sys.stderr)
+    # print("Time :",tt,file=sys.stderr)
+    if tt>=t:break
+    
+    if current==-1:
+        if pq_order==[]:
+            break
+        _,current=heappop(pq_order)
+        order_x,order_y,order_l=ol[current]
+        # print("Order :",current,file=sys.stderr)
+        
+    order_list=[]
+    for i in range(p):
+        for j in range(order_l[i]):
+            order_list+=[i]
+        
+    ware_best=-1
+    maxi=-1
+    dist_best=-1
+    load_best=[-1 for i in range(p)]
+    for ware in range(w):
+        # print("   Warehouse :",ware,end=" ",file=sys.stderr)
+        weight=0
+        load_temp=[0 for i in range(p)]
+        ware_x,ware_y,_=wl[ware]
+        ware_list=[wl[ware][2][produit] for produit in range(p)]
+        for produit in order_list:
+            #if ware_list[produit]>0:print("yolo",end=" ",file=sys.stderr)
+            if ware_list[produit]>0 and weight+pl[produit]<=pds:
+                weight+=pl[produit]
+                ware_list[produit]-=1
+                load_temp[produit]+=1
+        dist_temp=dist(a,b,ware_x,ware_y)+dist(ware_x,ware_y,order_x,order_y)
+        #print(weight,dist_temp,file=sys.stderr)
+        score=(1000*weight)/dist_temp
+        if score>maxi:
+            maxi=score
+            ware_best=ware
+            dist_best=dist_temp
+            for i in range(p):
+                load_best[i]=load_temp[i]
+    
+    if score==0.0:
+        current=-1
+        nnb+=1
+        # print("bye bye",sum(wl[0][2]),order_list,file=sys.stderr)
+        heappush(pq_drone,(tt,nd,a,b))
+    else:
+    
+        wait=dist_best
+        ware_x,ware_y,ware_list=wl[ware_best]
+        com2=""
+        for produit in range(p):
+            if load_best[produit]>0:
+                nb_com+=2
+                # print(str(nd)+" L "+str(ware_best)+" "+str(produit)+" "+str(load_best[produit]),file=sys.stderr)
+                # print(str(nd)+" D "+str(current)+" "+str(produit)+" "+str(load_best[produit]),file=sys.stderr)
+                com+=str(nd)+" L "+str(ware_best)+" "+str(produit)+" "+str(load_best[produit])+"\n"
+                com2+=str(nd)+" D "+str(current)+" "+str(produit)+" "+str(load_best[produit])+"\n"
+                wait+=2
+                ware_list[produit]-=load_best[produit]
+                order_l[produit]-=load_best[produit]
+                    
+        com+=com2
+        heappush(pq_drone,(tt+wait,nd,order_x,order_y))
+                    
+        if sum(order_l)==0:
+            current=-1
+        
+yy=sum(wl[0][2])
+        
 print(nb_com)
-print(com)
-	
+print(nb_com,file=sys.stderr)
+print(com,end="")
+# print(com,file=sys.stderr)
+# print(nnb,o,file=sys.stderr)
+# print(xx,yy,file=sys.stderr)
