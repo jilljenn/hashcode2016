d521aaa0b5c5f3f549121ec3be2416702cbb6e35
Jill-Jênn Vie <vie@jill-jenn.net>
2016-02-16 20:01:02 +0100

    Score depends now on last delivery in chronological order, not in output file order

--- checker.py
+++ checker.py
@@ -4,8 +4,8 @@ from math import sqrt, ceil
 
 FILE_IN = 'test.in'
 FILE_OUT = 'test.out'
-# FILE_IN = 'mother_of_all_warehouses.in'
-# FILE_OUT = 'output'
+FILE_IN = 'busy_day.in'
+FILE_OUT = 'bd.out'
 SCORE = 0
 
 lines = open(FILE_IN).read().splitlines()
@@ -34,6 +34,8 @@ coords_d = [coords_w[0]] * nb_drones
 time_d = [-1] * nb_drones
 load_d = [Counter() for _ in range(nb_drones)]
 
+delivery_times = {}
+
 def dist(c1, c2):
     return sqrt(pow(c1[0] - c2[0], 2) + pow(c1[1] - c2[1], 2))
 
@@ -54,7 +56,7 @@ with open(FILE_OUT) as f:
             load_d[drone_id][product_type] += quantity
             # TODO verify max_payload
             time_d[drone_id] += 1
-            print('Drone %d loads from warehouse %d at turn %d' % (drone_id, place_id, time_d[drone_id]))
+            # print('Drone %d loads from warehouse %d at turn %d' % (drone_id, place_id, time_d[drone_id]))
         else:
             time_d[drone_id] += ceil(dist(coords_d[drone_id], coords_o[place_id]))
             coords_d[drone_id] = coords_o[place_id]
@@ -62,7 +64,9 @@ with open(FILE_OUT) as f:
             load_d[drone_id][product_type] -= quantity
             request[place_id][product_type] -= quantity
             time_d[drone_id] += 1
-            if sum(request[place_id].values()) == 0:
-                print('Order %d is satisfied by drone %d at turn %d' % (place_id, drone_id, time_d[drone_id]))
-                SCORE += ceil((T - time_d[drone_id]) * 100 / T)
+            current_time = time_d[drone_id]
+            delivery_times.setdefault(place_id, []).append(current_time)
+            if all(quantity == 0 for quantity in request[place_id].values()):
+                # print('Order %d is satisfied by drone %d at turn %d' % (place_id, drone_id, time_d[drone_id]))
+                SCORE += ceil((T - max(delivery_times[place_id])) * 100 / T)
     print('SCORE:', SCORE)
